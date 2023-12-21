70efce1cfd925ca4b73c14fcc322058d8e482e19
Arya <aryailham05@gmail.com>
2023-12-21 23:55:41 +0700

    Update UserRemoteDataSource.swift

--- Project SALT/Module/Core/Data/Remote/UserRemoteDataSource.swift
+++ Project SALT/Module/Core/Data/Remote/UserRemoteDataSource.swift
@@ -6,10 +6,10 @@
 //
 
 import Foundation
+import Alamofire
 
-
-protocol LoginRemoteDataSource {
-    func login(email: String, password: String)
+protocol LoginRemoteDataSource: AnyObject {
+    func login(email: String, password: String, completion: @escaping ((Result<LoginResponse, Error>) -> ()))
 }
 
 class UserRemoteDataSource {
@@ -17,7 +17,28 @@ class UserRemoteDataSource {
 }
 
 extension UserRemoteDataSource: LoginRemoteDataSource {
-    func login(email: String, password: String) {
+    func login(email: String, password: String, completion: @escaping ((Result<LoginResponse, Error>) -> ())) {
+        let urlString = APIEndpoints.BASE_URL.rawValue + APIEndpoints.LOGIN_ENDPOINT.rawValue
+        
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        
+        let parameters: Parameters = [
+            "password": password,
+            "email": email,
+        ]
+        
+        AF.request(url, method: .post, parameters: parameters).responseDecodable(of: LoginResponse.self) { response in
+            switch response.result {
+            case .success(let success):
+                completion(.success(success))
+                break
+            case .failure(let failure):
+                completion(.failure(failure))
+                break
+            }
+        }
         
     }
 }
