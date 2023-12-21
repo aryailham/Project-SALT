edaebedba22c7b5b821e3f3da86125964564f87b
Arya <aryailham05@gmail.com>
2023-12-21 23:07:19 +0700

    Create API KEY.swift

---
+++ Project SALT/Module/Common/API KEY.swift
@@ -0,0 +1,15 @@
+//
+//  API KEY.swift
+//  Project SALT
+//
+//  Created by Arya Ilham on 21/12/23.
+//
+
+import Foundation
+
+enum API_KEY: String {
+    case BASE_URL = "https://reqres.in/"
+    
+    // MARK: - USER API
+    case LOGIN_ENDPOINT = "/api/login"
+}
